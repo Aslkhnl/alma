0adc25771d235be312ef14a0703c39204d07e2b1
Ayauly Seiikhan <ayaulyseiikhan@Ayaulys-MacBook-Air.local>
2018-02-28 21:38:07 +0600

    first commit

--- database.php
+++ database.php
@@ -1,16 +1,21 @@
 <?php
-    
-    require_once('db_credentials.php');
-    
+
+    define("DB_SERVER", "localhost");
+    define("DB_USER", "ayauly");
+    define("DB_PASS", "1q2w3e4R");
+    define("DB_NAME", "almau_curriculum.sql");
+
     function db_connect() {
         $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
         return $connection;
     }
-    
+    $sql = "SELECT * FROM course ";
+    $sql .= "ORDER BY position ASC";
+
     function db_disconnect($connection) {
         if(isset($connection)) {
             mysqli_close($connection);
         }
     }
-    
+
     ?>
